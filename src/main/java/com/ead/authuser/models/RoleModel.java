e3018e8249b323ac88283fea6f766693ae3f805d
Eduardo José <duduxss3@gmail.com>
2022-04-15 18:07:20 -0300

    master
    	:rocket: Setting up authuser authentication
    
    		 - Creating new table Role and putting some roles together
    		 - Creating WebSecurityConfigClass
    		 - Creating Role Service, Service Impl, Repository, Role Type

---
+++ src/main/java/com/ead/authuser/models/RoleModel.java
@@ -0,0 +1,44 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.RoleType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.UUID;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class RoleModel implements GrantedAuthority, Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private UUID id;
+
+    @Enumerated(STRING)
+    @Column(nullable = false, unique = true, length = 30)
+    private RoleType name;
+
+    @Override
+    @JsonIgnore
+    public String getAuthority() {
+        return this.name.toString();
+    }
+
+}
